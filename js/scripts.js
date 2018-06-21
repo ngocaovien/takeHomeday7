4f06c8cc3bc031a39972894adf247a1caa3abc25
ngocaovien <ngocaovien@gmail.com>
2018-06-20 18:40:11 -0700

    done

--- js/scripts.js
+++ js/scripts.js
@@ -1,202 +1,172 @@
-// // let body = document.getElementsByTagName('body'); //the plural will return an Array, single (element) will return one
-// // console.log(body[0]);// use the index of body to grab th body only. older method
-// //
-// // let classes = document.getElementsByClassName('my-header'); //grabs every class with my-header and remove that class from all of them.
-// // console.log(classes[0]);
-// //
-// // let id = document.getElementById("my-paragraph"); //ID should never be used for styling, used only fo Javascript.
-// // console.log(id);
-// //
-// // //NEWER METHODS are below.
-// //
-// // let tag = document.querySelector('body') //querySelector will only return on element. only give back the first one if multiple.
-// // console.log(tag);
-// //
-// // let myClass = document.querySelector('.my-header'); //will only give you back the first class of my-header
-// // console.log(myClass);
-// //
-// // let myId =document.querySelector('#my-paragraph');
-// // console.log(myId);
-// //
-// // let tags = querySelectorAll('body'); //will always return an ARRAY with all of the tag involved. use period of class and # for ID.
+// //day 8 all about form
 //
-// // function queryDom(methodName, element){
-// //   switch (methodName.tolowercase()) {
-// //     case "getElementsByTagName":
-// //       console.log(document.getElementsByTagName(element));
-// //       break;
-// //     case "getElementById":
-// //         console.log(document.getElementById(element));
-// //         break;
-// //     case "getElementsByClassName":
-// //           console.log(document.getElementsByClassName(element));
-// //           break;
-// //     case "querySelector":
-// //             console.log(document.querySelector(element));
-// //             break;
-// //     case "querySelectorAll":
-// //               console.log(document.querySelectorAll(element));
-// //               break;
-// //     default:
-// //     break;
-// //   }
+// //type input for form
+// //type: number, checkbox, text...etc see the html.
+//
+// //put the input in a form.
+//
+// // console.log(document.forms); // to see what's in it. the input becomes a Child nodes.
+// // console.log(document.forms.login); //we get that childnode only.
+// //we can set name to the form.
+// //form validation. write function to check values that are entered in.
+//
+// //input also have name attribute.
+//
+//
+//
+// // console.log(document.forms.login.email); //will show this the input with the anem email.
+// //
+// // console.log();
+// //   let passwordInput = document.forms.login.password;
+// //   let emailInput = document.forms.login.email;
+// //   let myPTag = document.querySelector('#data_binding');
+// //   let submit = document.forms.login.submitBtn;
+// //
+// //   emailInput.addEventListener('keyup' , e => { //when emailInput happens, listen to this..
+// //   myPTag.textContent = e.target.value; //might use keycode for enter to do something.3 types, keydown, keyup, and keycode.
+// // });
+// //   submit.addEventListener('click' , e =>{ //keeps it from submitting if not filled in.
+// //     if (passwordInput.value !== '' || emailInput.value !=='') {
+// //       submit.disabled = false);
+// //     }
+// //   })
+//   // submit.addEventListener('click' , e =>{ //keeps it from submitting if not filled in.
+//   //   if (passwordInput.value === '' || emailInput.value ==='') {
+//   //     e.preventDefault();
+//   //   }
+//
+// // "e.target.value ==> looks at what we type."
+// //forms create their own scope in javascript.
+//
+// //how do we use forms? ACTION and METHOD.
+// //ACTION attribute asks what URL on the back end do you want to send this information.
+// // inside the event listener, we
+//
+// //What's this? what's this?
+// //inside any function that you write, you will need a reference the function that you called.
+// //you need to have access to a dynamic reference of a function.
+// //how a function is called and where a function is called is two different thing.
+// //this.name = "henry"; //this will always refers to the global scope.
+//
+//  // function logMyName() { //unless you're in a fat arrow function, this will always refer to the global scope.
+//  //   this.name ="John";
+//  //   console.log(this);
+//  // }
+// //  let user = { //let var and const are no bound to 'this'
+// //    name: "henry",
+// //    password: 12345,
+// //    status: "active",
+// //  };
+// // let guest = {
+// //   name : "guest",
+// //   password : "undefined",
+// //   status: "inactive"
 // // }
 // //
-// // let answer = queryDom(getElementsByTagName, body);
+// //  function logMyName(greeting, ado) {
+// //    console.log(this.name, this.password);
+// //    alert(`${greeting}, ${this.name}!`);
+// //  }
 // //
+// // logMyName.call(user);//call site, with call, we call henry, and not the regular this.
+// // logMyName.call(guest);
+// // //use call or apply to change the context of this. use the same function but with different context.
 // //
-// // //ryeker version
+// // logMyName.call(user, "Hello");
+// // logMyName.call(guest, "Hello"); //first argument is the THIS argument call will change. the next argument is the argument for the function.
 // //
-// // function queryDom(methodName, element){
-// //   switch(methodName.tolowercase()){
-// //     case 'getelementsbytagname':
-// //       document.getElementsByTagName(element);
-// //       break;
-// //     case 'queryselector':
-// //       document.querySelector(`.${element}`) //need a way to determine if it's a class or ID, add a second method or add anothe argument.
-// //   }
-// // }
+// // //APPLY works the exact same way, but the info we're feeding in is an array. EXPLICIT BINDING
+// // logMyName.apply(user, ["Hello", "Goodbye"]);
+// // logMyName.apply(guest, ["Hello"]);
 //
-// // let myClass = document.querySelector('.my-header'); //grabs every class with my-header and remove that class from all of them.
-// //
-// // myClass.addEventListener(
-// //   'click', //string that is a valid event.
-// //   e => { //and takes a function, that is a call back function. e is a placeholder, stands for event.
-// //     alert("hi");
-// // });
-// //
-// // console.log(myClass);
+// //EXPLICIT BINDING with dot this, dot that.
 //
-// let myButton = document.querySelector(".my-button");
-// let myDiv = document.querySelector('.my-div');
-// let h1 =document.querySelector('h1');
+// //IMPLICIT binding, calling a function with a certain  context.
 //
-// // myButton.addEventListener(
-// //   'click',
-// //   e => {
-// //     alert("the button has been clicked.");
-// //   });
+// let newScope = {
+//   name: "Peter Piper",
+//   instrument: "Pipes",
+//   password: "bag of peppers",
+//   context: logMyName //last set of key value does not neeed a comma.
+// }
+//
+// newScope.context("fky away");// whatever the key that we're setting our function to. here we set it with context. Content is now a function of logMyName
+//
+// //WHY would we use implicit binding over explicit binding? Call and apply is used if we want to bind it quickly and does not have access to that scope.
+//
+// //object oriented programming. all things are and object and all the object are related to each other.
+//
+// //lets relate this to the final. the submit button will be able to place it into the ul. do it tonight.
+// //you will need a reference in JS to bind that list.
+// //this.duedate is a certain date. you would need to bind this to a point in your code.
+// //changing stuff on the dom.
+// //edit complete delete will need to reflect the ul. this.status === done
+// //use this binding to change the way
 //
 //
-// myButton.addEventListener( //.textcontent example
-//   'click',
-//   e => {
-//   e.target.textContent = "DO NOT CLICK";
-//   });
 //
+// let classScore = 90; //global cannot see into scopeOne and scopeTwo
 //
-// myDiv.addEventListener(
-//   'mouseenter',
-//   e => {
-//     e.target.style.backgroundColor = "#ffff00";
-//   });
+//  { //the scope protects the variable inside of it.
+//   let classScore= 75;
+//   console.log(classScore, "first scope");
+// {
+//     let classScore=50;
+//     console.log(classScore, "second scope");
 //
-// h1.addEventListener(
-//   'mouseleave',
-//   e => {
-//     e.target.style.color = "#00ffff";
-//   });
-//
-// //innerHTML. Always use textcontent..only value is a string to enter
-// let div = document.querySelector('div');
-// div.innerHTML = "<p>lorem ispsum dolorus cantu!!</p>"; //create this element in html
-//
-// //createElement
-//
-// let newH1 = document.createElement("h1"); //set it to a varible.
-// const body =document.querySelector("body");
-//
-// newH1.textContent = "Hello Baby"; //add something into it.
-// body.appendChild(newH1);
-//
-//
-// console.log(newH1);
-//
-// //insert before, needs 3 things (parent, new node, and reference node )
-// newH1.className = "text-center";//best practice to have the newH1 style right before it.///but will replace whats there
-// body.insertBefore(newH1, div);
-//
-// //elegant way to do e.target.style blah
-// div.id ="red_text"; //make sure to have the ID in css.
-//
-// //classList; a method of it's own. multiple arguments that you can give.
-// div.classList.add("text-center"); //does not override what is already there. we add to it.
-// //use classList to add class to newly made element.
-//
-// //next classList type is REMOVE.
-//
-// // div.addEventListener( 'click', e=>{
-// //   div.classList.remove("text-center");
-// // });
-//
-// //toogle() turn on and off
-// // div.addEventListener( 'click', e=>{
-// //   div.classList.toggle("text-center");
-// // });
-//
-// //contains, tells you if a class exist or not.
-// div.addEventListener( 'click', e=>{
-//   div.classList.toggle("text-center");
-//   console.log(div.classList.contains("text-center"));
-//   div.classList.replace("blue-background", "green-background");
-// });
-//
-// myImg = document.createElement('img'); //create img tag
-//
-// myImg.setAttribute('src', "https://cdn3.tmbi.com/secure/RMS/attachments/37/1200x1200/Crispy-Fried-Chicken_exps6445_PSG143429D03_05_5b_RMS.jpg");
-//
-// div.appendChild(myImg);//add it to div element.
-
-
-//Fizz buzz
-
-// for ( i=1; i <= 100; i++){
-//   let body = document.querySelector("body");
-//   let p = document.createElement('p');
-//
-//   if (i % 3 && i % 5 === 0){
-//     p.textContent = "fizzbuzz";
-//   } else {
-//     if (i % 5 === 0){
-//     p.textContent = "buzz";
-//   }else{
-//     if (i % 3 === 0){
-//       p.textContent = "fizz";
-//     } else{
-//       p.textContent = i ;
-//     }
-//     body.appendChild(p);
 //   }
-// }
-// }
+// };
+//
+// console.log(classScore, "global scope");
+
+//global scope does it changed with var. let does not have that problem.
+
+//day 9 Brandy's teaching.
+
+//how to create a new object:
+
+let object1 ={} //how to define an empty object.
+
+let object2 = new Object() //another way to create an empty oject.
 
 
-//day 7 takeHome
-let myButton = document.querySelector(".my-button");
-let body = document.querySelector("body");
+let person = { //each object will have key and value.
+  firstName : "Brandy", //key is firstName. we can have as many as we want. value can be any data type
+  lastName: "Moreno",
+  age: 22,
+  favorites: ["cookies" , "pizza"],
+  desc : { //we can put an object inside and object.
+      hairColor : "brown",
+  }
+}
 
-myButton.addEventListener(
-  'click',
-  e => {
-    var title = prompt("What's the title?" , "Rick and Morty");
-    var doToday = prompt("What did you do today?" , "Made fried chicken.");
-    var color = prompt("What is your favorite color" , "Purple");
-    color = color.toLowerCase();
-    console.log(color);
+//how do i put value into an object?
 
-    var att = document.createAttribute("class");
-    att.value = "text-center";
-    let h1 = document.createElement('h1');
-    h1.setAttributeNode(att);
-    h1.textContent = title;
-    body.appendChild(h1);
+object1.food = "pizza"; //this is how we add to an empty obj. this is call dot notation.
+object1["color"] = "pink"; // bracket notation.
 
-    var att = document.createAttribute("class");
-    att.value = "justify";
-    let p = document.createElement('p');
-    p.setAttributeNode(att);
-    p.textContent = doToday;
-    body.appendChild(p);
+object1.age;// how look for something inside an object.
 
-    document.querySelector('p').style.backgroundColor = "red";
-  });
+for (let key in person ){//use loops to go over the object/to search them for a value. this type is "for in"
+  // console.log('key' , key) //need key to access items in object.
+  console.log(person.key);
+};
+
+//for .. of
+
+for(let key of Object.keys(person)){ //for of
+  console.log(key);
+}
+
+
+for(let [key, value] of Object.entries(person)){ //for of needs the OBJECT command..for of can be used on arrays.
+  console.log(key, value);
+}
+
+
+//to clone an object
+
+let newObj = Object.create(object1) //objects are saved by their reference in memory.spot in memory.
+newObj.flower = "rose";
+console.log(newObj);
+newObj.color = 'yellow';
